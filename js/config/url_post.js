021eb9a6defe8c68e30823fd7ba05adb148c1c9e
Indra Riksa Herlambang <indrariksa@gmail.com>
2024-05-29 22:35:30 +0700

    post

---
+++ js/config/url_post.js
@@ -0,0 +1,7 @@
+export let urlPOST = "http://127.0.0.1:8080/insert"
+
+export function AmbilResponse(result) {
+    console.log(result); //menampilkan response API pada console
+    alert(result.message); //menampilkan response API pada alert
+    window.location.reload(); //reload halaman setelah klik ok pada alert
+}
