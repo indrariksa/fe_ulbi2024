22c5ae9af465fa6b7a67306e7b8923574b4c659a
Indra Riksa Herlambang <indrariksa@gmail.com>
2024-06-06 23:04:14 +0700

    up update

---
+++ js/config/url_put.js
@@ -0,0 +1,10 @@
+const urlParams = new URLSearchParams(window.location.search);
+const presensiId = urlParams.get('presensiId');
+
+export let urlPUT = "https://ws-indra2024-878f7e6fab92.herokuapp.com/update/" + presensiId;
+
+export function AmbilResponse(result) {
+    console.log(result); //menampilkan response API pada console
+    alert(result.message); //menampilkan response API pada alert
+    window.location.href = "index.html"; //reload halaman setelah klik ok pada alert
+}
